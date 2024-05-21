329e978cac988e26bf602d95be75b5502c3eed24
Erick Renaud <renaud_junior@outlook.com>
2024-05-21 00:58:52 -0400

    added warning message when user doesnt give all inputs.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+//easeOfUseFeature
+const modalWarningMessage = $('<p>').text('Please make sure to fill out all three input boxes!!').addClass('text-danger fw-bold');
+const modalForm = $('form');
 // Todo: create a function to generate a unique task id
 function generateTaskId(task) {
     if(!nextId) {
@@ -113,6 +116,18 @@ function handleAddTask(event){
     newTask.Id = newTaskId;
     //create the card here for now to visualize changes
     // createTaskCard(newTask);
+
+    //making sure that all elements of the tasks are filled out
+    
+    for (attributes in newTask) {
+        if(!newTask[attributes]) {
+            modalForm.append(modalWarningMessage);
+            event.preventDefault();
+            return;
+        }
+    }
+
+    $('#submitTask').attr('data-bs-dismiss', 'modal');
     
     if(!taskList) {
         taskList = [];
@@ -127,7 +142,6 @@ function handleAddTask(event){
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-    console.log(event)
     var x = 0;       
     for (var task of taskList) {
         if (task.Id == event.target.id){
@@ -163,8 +177,10 @@ const addTask = $('#formModal');
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
     renderTaskList();
+
     addTask.on('submit', handleAddTask);
     $('button').click(handleDeleteTask);
+
     $( function() {
         $( "#datepicker" ).datepicker();
         $( "#sortable1, #sortable2, #sortable3" ).sortable({
